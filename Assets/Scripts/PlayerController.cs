8ae46624e8b5352e18b3ac885b7d697fd842176a
Shiv Kampani <shivkampani@gmail.com>
2023-02-25 13:13:25 -0500

    Broken Curved Code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,13 +11,19 @@ public class PlayerController : MonoBehaviour
 
     private const float C = 6.0f;
 
-    private float smoothValue = 0.2f;
-    private float currentVelocity = 4.95f;
+    private float smoothValue = 0.5f;
+    private float peakVelocity = 4.0f;
     private float xRotation = 0.0f;
     private float yRotation = 0.0f;
 
     private float maxXRotation = 40.0f;
     private float sensitivity = 750.0f;
+
+    private int bufferSize = 100;
+    private Vector4[] positionBuffer;
+    private int bufferHead = 0;
+    private float timeKeeper = 0.0f;
+    private float timeResolution = 0.2f;
 
     private Vector3 instantVelocity;
     private Vector3 refVelocity = Vector3.zero;
@@ -27,16 +33,42 @@ public class PlayerController : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         controller = this.gameObject.GetComponent<CharacterController>();
         instantVelocity = new Vector3(0.0f, 0.0f, 0.0f);
+        ClearBuffer();
     }
 
-    void PassSpeed() {
-        // Passes the instantaneous velocity to the materials;
-        float v = instantVelocity.magnitude;
-        float beta = v / C;
-        CubeMaterial.SetFloat("_Beta", beta);
-        GroundMaterial.SetFloat("_Beta", beta);
+    void ClearBuffer() {
+        positionBuffer = new Vector4[bufferSize];
+        for (int i=0; i<bufferSize; i++)
+            positionBuffer[i] = new Vector4(transform.position.x, transform.position.y + 0.5f, transform.position.z, 1.0f);
     }
 
+    void TrackPosition() {
+        // Pass all prior positions of player back to array at 0.2 time intervals
+        // In shader script, convert original position of player to camera basis 
+        // Update positions of mesh vertices accordingly
+        if (timeKeeper >= timeResolution) {
+            bufferHead = (bufferHead + 1) % bufferSize;
+            positionBuffer[bufferHead] = new Vector4(transform.position.x, transform.position.y + 0.5f, transform.position.z, 1.0f);
+            timeKeeper -= timeResolution;
+        }
+        timeKeeper += Time.deltaTime;
+    }
+
+    void PassKinematics() {
+        // Pass the instantaneous velocity to shader
+        // float v = instantVelocity.magnitude;
+        Vector3 beta = instantVelocity / C;
+        Vector4 paddedBeta = new Vector4(beta.x, beta.y, beta.z, 0.0f);
+        Shader.SetGlobalFloat("_C", C);
+        Shader.SetGlobalVector("_Beta", paddedBeta);
+        // Pass the previous positions to shader
+        Debug.Log(positionBuffer[bufferHead] + " " + bufferHead);
+        Shader.SetGlobalFloat("_TimeResolution", timeResolution);
+        Shader.SetGlobalInt("_BufferHead", bufferHead);
+        Shader.SetGlobalVectorArray("_PositionBuffer", positionBuffer);
+    }
+
+    // Returns the Look Direction
     void Look() {
         float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
@@ -60,7 +92,7 @@ public class PlayerController : MonoBehaviour
             xVelocity = 1.0f;
         targetVelocity = (xVelocity * Vector3.Scale(transform.right, new Vector3(1, 0, 1)) + zVelocity * Vector3.Scale(transform.forward, new Vector3(1, 0, 1)));
         targetVelocity.Normalize();
-        targetVelocity *= currentVelocity;
+        targetVelocity *= peakVelocity;
         instantVelocity = Vector3.SmoothDamp(instantVelocity, targetVelocity, ref refVelocity, smoothValue);
         controller.Move(instantVelocity * Time.deltaTime);
     }
@@ -69,6 +101,7 @@ public class PlayerController : MonoBehaviour
     {
         Look();
         Move();
-        PassSpeed();
+        TrackPosition();
+        PassKinematics();
     }
 }
