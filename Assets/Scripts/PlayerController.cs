c46778bef1e4cd5f51f4ef0f88d7ce4c5921137e
Shiv Kampani <shivkampani@gmail.com>
2023-02-25 00:08:45 -0500

    Fixed CharacterController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,33 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float smoothValue = 0.5f;
+
+    private CharacterController controller;
+
+    private float smoothValue = 0.2f;
     private float currentVelocity = 5.0f;
-    private float rotationStep = 30.0f;
+    private float xRotation = 0.0f;
+    private float yRotation = 0.0f;
+
+    private float maxXRotation = 40.0f;
+    private float sensitivity = 750.0f;
 
     private Vector3 instantVelocity;
     private Vector3 refVelocity = Vector3.zero;
     
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        controller = this.gameObject.GetComponent<CharacterController>();
         instantVelocity = new Vector3(0.0f, 0.0f, 0.0f);
-        Debug.Log("Entered Player Controller.");
     }
 
     void Look() {
-        // Basic Rotate Camera
-        if (Input.GetKey(KeyCode.LeftArrow))
-            transform.eulerAngles -= new Vector3(0, rotationStep, 0) * Time.deltaTime;
-        if (Input.GetKey(KeyCode.RightArrow))
-            transform.eulerAngles += new Vector3(0, rotationStep, 0) * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        xRotation -= mouseY;
+        yRotation += mouseX;
+        xRotation = Mathf.Clamp(xRotation, -maxXRotation, maxXRotation);
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0.0f);
     }
 
     void Move() {
         Vector3 targetVelocity;
         float xVelocity = 0.0f;
         float zVelocity = 0.0f;
-
-        // Smooth Move Object
         if (Input.GetKey(KeyCode.W))
             zVelocity = 1.0f;
         if (Input.GetKey(KeyCode.A))
@@ -39,12 +46,11 @@ public class PlayerController : MonoBehaviour
             zVelocity = -1.0f;
         if (Input.GetKey(KeyCode.D))
             xVelocity = 1.0f;
-
-        targetVelocity = new Vector3(xVelocity, 0.0f, zVelocity);
+        targetVelocity = (xVelocity * Vector3.Scale(transform.right, new Vector3(1, 0, 1)) + zVelocity * Vector3.Scale(transform.forward, new Vector3(1, 0, 1)));
         targetVelocity.Normalize();
         targetVelocity *= currentVelocity;
         instantVelocity = Vector3.SmoothDamp(instantVelocity, targetVelocity, ref refVelocity, smoothValue);
-        transform.position += (instantVelocity * Time.deltaTime);
+        controller.Move(instantVelocity * Time.deltaTime);
     }
     
     void Update()
